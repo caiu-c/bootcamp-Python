3631f03454f977eabdea64820949563d35c078e6
Caiuc <contato.caiuc@gmail.com>
2025-04-21 14:33:56 -0300

    Mudança: mudanças de sintaxe nos inputs do exercicio

--- aula01/exercicios.py
+++ aula01/exercicios.py
@@ -6,7 +6,7 @@ print("Seu nome tem ", len(nome), "caracteres.")
 
 # Printa soma dois valores fornecidos pelo usuário.
 
-num1 = float(input("Digite o primeiro número"))
-num2 = float(input("Digite o segundo número"))
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
 
 print(f"A soma dos dois números é: {num1 + num2}")
