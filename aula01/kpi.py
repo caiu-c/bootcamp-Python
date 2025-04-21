77a6ad3e5ffa5c536bf7c500184dd636753a76bb
Caiuc <contato.caiuc@gmail.com>
2025-04-21 15:07:26 -0300

    Adding KPI file to calculate the employee bonus

---
+++ aula01/kpi.py
@@ -0,0 +1,10 @@
+nome = input("Olá, qual é o seu nome? ")
+
+salario = input(f"Muito bem, {nome}. Agora me diga o seu salário: ")
+salario = float(salario)
+bonus = input(f"Entendi, {nome}. R${salario} é um bom salário!\nQual o bônus desejado? ")
+bonus = float(bonus)
+
+KPI = salario * bonus
+
+print(f"Olá {nome}, o seu valor bônus foi de {KPI}.")
