a6775cce37ea965be4968473b51ad150a0d86437
Caiuc <contato.caiuc@gmail.com>
2025-04-21 15:35:27 -0300

    error: I missunderstood a calculation rule. So, I'm doing it right now

--- aula01/kpi.py
+++ aula01/kpi.py
@@ -5,6 +5,6 @@ salario = float(salario)
 bonus = input(f"Entendi, {nome}. R${salario} é um bom salário!\nQual o bônus desejado? ")
 bonus = float(bonus)
 
-KPI = salario * bonus
+KPI = 1000 + (salario * bonus)
 
 print(f"Olá {nome}, o seu valor bônus foi de {KPI}.")
