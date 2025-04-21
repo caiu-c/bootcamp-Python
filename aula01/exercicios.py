81ee579c184cf129c4549cc0b918f9b4c34b39c1
Caiuc <contato.caiuc@gmail.com>
2025-04-21 14:31:51 -0300

    Adicionando o primeiro exercício

---
+++ aula01/exercicios.py
@@ -0,0 +1,12 @@
+# Printa a quantidade de caracteres do nome do usuário.
+
+nome = input("Qual é o seu nome? ")
+
+print("Seu nome tem ", len(nome), "caracteres.")
+
+# Printa soma dois valores fornecidos pelo usuário.
+
+num1 = float(input("Digite o primeiro número"))
+num2 = float(input("Digite o segundo número"))
+
+print(f"A soma dos dois números é: {num1 + num2}")
